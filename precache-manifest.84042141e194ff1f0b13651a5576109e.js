0236d427945f5e6c309454e57fd03a99ebc4d385
pksuarez <pablokalet@gmail.com>
2020-06-21 15:41:35 -0600

    Updates

---
+++ precache-manifest.84042141e194ff1f0b13651a5576109e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44ec9490c20bdd81ed9991c6e34c391",
+    "url": "/piramide-usuarios/index.html"
+  },
+  {
+    "revision": "ef001c018fca97d2566b",
+    "url": "/piramide-usuarios/static/css/main.dd6e4ef8.chunk.css"
+  },
+  {
+    "revision": "0496b541e198a98fca4c",
+    "url": "/piramide-usuarios/static/js/2.8a3f819e.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/piramide-usuarios/static/js/2.8a3f819e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef001c018fca97d2566b",
+    "url": "/piramide-usuarios/static/js/main.08f20080.chunk.js"
+  },
+  {
+    "revision": "e9bd8cb97e52fe56cb78",
+    "url": "/piramide-usuarios/static/js/runtime-main.772e4478.js"
+  }
+]);
